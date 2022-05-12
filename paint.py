9ba8562d44be538a5a1a669329fba5d974c9f538
Carelia Ibarra <A01251463@tec.mx>
2022-05-12 00:28:37 -0700

    Actividad 1: Paint

---
+++ paint.py
@@ -0,0 +1,115 @@
+'''Maria Carelia Ibarra Vasquez
+A01251463
+Herramientas computacionales: el arte de la programacion TC1001S.201
+11 de mayo del 2022'''
+
+from math import *
+
+from turtle import *
+
+from freegames import vector
+
+
+def line(start, end):
+    """Draw line from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
+
+
+def square(start, end):
+    """Draw square from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
+
+
+def circumference(start, end):
+    """Draw circle from start to end."""
+
+    up()
+    goto(start.x, (start.y + (start.y / 2)))
+    down()
+    begin_fill()
+
+    radius = sqrt(pow((start.x - end.x), 2) + pow((start.y - end.y), 2))
+    circle(radius)
+    
+    end_fill()
+
+
+def rectangle(start, end):
+    """Draw rectangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count % 2 == 0:
+            forward(start.x)
+            left(90)
+        else:
+            forward(end.x)
+            left(90)
+
+    end_fill()
+
+
+def triangle(start, end):
+    """Draw triangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(45)
+
+    end_fill()
+
+
+def tap(x, y):
+    """Store starting point or draw shape."""
+    start = state['start']
+
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+
+
+def store(key, value):
+    """Store value in state at key."""
+    state[key] = value
+
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+onkey(undo, 'u')
+onkey(lambda: color('black'), 'K')
+onkey(lambda: color('white'), 'W')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
+onkey(lambda: store('shape', line), 'l')
+onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', circumference), 'c')
+onkey(lambda: store('shape', rectangle), 'r')
+onkey(lambda: store('shape', triangle), 't')
+done()
